c587d71f8d8ce022a9a89bd74b4fdce6c5c09aa3
Tim <tim.fink519@gmail.com>
2022-06-18 17:28:44 -0400

    links

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+const h1 = document.createElement("h1");
+h1.textContent = "Shelby Company Limited";
+document.querySelector("body").appendChild(h1);
 const h2 = document.createElement("h2");
-h2.textContent = "Shelby Company Limited";
+h2.textContent = "Established in Birmingham, England in 1918";
 document.querySelector("body").appendChild(h2);
-const h3 = document.createElement("h3");
-h3.textContent = "Established in Birmingham, England in 1918";
-document.querySelector("body").appendChild(h3);
