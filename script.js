a472c0fdb0e14d02b44bfef8aff41052bdc90f6c
Tim <tim.fink519@gmail.com>
2022-06-18 17:49:04 -0400

    responsive topnav

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ document.querySelector("body").appendChild(h1);
 const h2 = document.createElement("h2");
 h2.textContent = "Established in Birmingham, England in 1918";
 document.querySelector("body").appendChild(h2);
+
+function myFunction() {
+    let x = document.getElementById("myTopmenu");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
