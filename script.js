6a248f2d6565ced9ff2b36c683d90a80fc50628c
Tim <tim.fink519@gmail.com>
2022-06-19 19:04:25 -0400

    scrolleffect

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 const h1 = document.createElement("h1");
 h1.textContent = "Shelby Company Limited";
-document.querySelector("body").appendChild(h1);
-const h2 = document.createElement("h2");
-h2.textContent = "Established in Birmingham, England in 1918";
-document.querySelector("body").appendChild(h2);
-
-function myFunction() {
-    let x = document.getElementById("myTopmenu");
-    if (x.className === "topnav") {
-        x.className += "responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+document.querySelector("div").appendChild(h1);
